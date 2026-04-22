a133a703ab001024a852c40615cfe3d752961179
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-04-23 00:45:55 +0800

    Initialise frontend pages - Landing page - User signup page - User login page - Admin login page - Dashboard user page - Dashboard user css

---
+++ trustpay/app/dashboard/user/page.tsx
@@ -0,0 +1,169 @@
+"use client";
+
+import { useState } from "react";
+import "./dashboard_user.css";
+
+type Transaction = {
+    id: number,
+    toName: string;
+    toPhone: string;
+    amount: number;
+    status: "NORMAL" | "SUSPICIOUS" | "FLAGGED";
+};
+
+export default function UserDashboard() {
+
+    const [showForm, setShowForm] = useState(false);
+
+    const [toName, setToName] = useState("");
+    const [toPhone, setToPhone] = useState("");
+    const [amount, setAmount] = useState("");
+
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+    const handleSubmit = async () => {
+        const res = await fetch("/api/transfer", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                toName,
+                toPhone,
+                amount: Number(amount),
+            }),
+        });
+
+        const result = await res.json();
+
+        let status = result.status
+
+        if (status === "FLAGGED") {
+            alert("Transaction rejected: This transaction account was flagged and not allowed to transfer.");
+            setShowForm(false);
+            return;
+        }
+
+        if (status === "SUSPICIOUS") {
+            alert("Transaction suspicious: Waiting admin to validate");
+        }
+
+        if (status === "NORMAL") {
+            alert("Transaction Completed!");
+        }
+
+        // Save transaction
+        setTransactions((prev) => [
+            {
+                id: Date.now(),
+                toName,
+                toPhone,
+                amount: Number(amount),
+                status,
+            },
+            ...prev,
+        ]);
+
+        // Reset the form
+        setShowForm(false);
+        setToName("");
+        setToPhone("");
+        setAmount("");
+    };
+
+    return (
+        <div className="dashboard-user">
+
+            <h1 className="dashboard-title">
+                TrustPay Transaction View
+            </h1>
+
+            <button
+                onClick={() => setShowForm(true)}
+                className="btn-primary"
+            >
+                + Add New Transaction
+            </button>
+
+            {/* No transaction made before */}
+            {transactions.length === 0 ? (
+                <p className="text-gray-500">
+                    No transaction record found.
+                </p>
+
+            ) : (
+                <div className="transaction-list">
+
+                    {transactions.map((tx) => (
+                        <div key={tx.id} className="border p-4 rounded">
+                            <p>To: {tx.toName}</p>
+                            <p>Phone: {tx.toPhone}</p>
+                            <p>Amount: RM {tx.amount}</p>
+
+                            <p
+                                className={
+                                    tx.status === "FLAGGED"
+                                        ? "text-red-600"
+                                        : tx.status === "SUSPICIOUS"
+                                            ? "text-orange-500"
+                                            : "text-green-600"
+                                }
+                            >
+                                {tx.status}
+                            </p>
+
+                        </div>
+                    ))}
+
+                </div>
+            )}
+
+            {/* form */}
+            {showForm && (
+                <div className="modal-bg">
+                    <div className="modal-box">
+
+                        <h2 className="text-xl font-bold mb-4">
+                            New Transaction
+                        </h2>
+
+                        <button
+                            onClick={() => setShowForm(false)}
+                            className="absolute top-3 right-3 text-black-600 hover:text-black text-xl"
+                        >
+                            x
+                        </button>
+
+                        <input
+                            className="input"
+                            placeholder="Recipient Name"
+                            value={toName}
+                            onChange={(e) => setToName(e.target.value)}
+                        />
+
+                        <input
+                            className="input"
+                            placeholder="Phone Number"
+                            value={toPhone}
+                            onChange={(e) => setToPhone(e.target.value)}
+                        />
+
+                        <input
+                            className="input"
+                            placeholder="Amount($)"
+                            type="number"
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                        />
+
+                        <button
+                            onClick={handleSubmit}
+                            className="bg-blue-600 text-white px-3 py-1 rounded"
+                        >
+                            Transfer
+                        </button>
+
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
---
+++ trustpay/app/login/admin/page.tsx
@@ -0,0 +1,102 @@
+"use client"
+import Link from "next/link";
+import { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useRouter } from "next/navigation";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+
+export default function LoginPage() {
+    const router = useRouter();
+
+    const handleLogin = async (e: any) => {
+        e.preventDefault();
+
+        const form = e.currentTarget;
+
+        const email = form.email.value;
+        const password = form.password.value;
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            router.push("/dashboard/user");
+        } catch (error) {
+            alert("Fail to login");
+        }
+    };
+
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <div className="page-container">
+            <div className="image-section">
+                <img
+                    src="/title.png"
+                    alt="signup illustration"
+                    className="w-full h-full object-cover"
+                />
+            </div>
+
+            <div className="form-right">
+                <div className="form">
+
+                    <h1 className="title">WELCOME</h1>
+                    <p className="subtitle">Login with your details.</p>
+
+                    <form className="flex flex-col gap-2">
+                        <label htmlFor="email" className="text-sm font-medium block">
+                            Email
+                        </label>
+
+                        <input
+                            type="email"
+                            id="email"
+                            placeholder="Enter your email"
+                            className="input-field"
+                        />
+
+                        <div className="flex items-center justify-between text-sm">
+                            <label className="flex items-center">
+                                <input type="checkbox" className="w-4 h-4 accent-blue-600" />
+                                Remember me
+                            </label>
+                        </div>
+
+                        <label htmlFor="password" className="text-sm font-medium block">
+                            Password
+                        </label>
+                        <div className="password">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="password"
+                                placeholder="Enter your password"
+                                className="input-field"
+                            />
+                            <button
+                                type="button"
+                                className="absolute right-2 top-1/3 -translate-y-1/2 text-gray-500"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
+                            </button>
+                        </div>
+
+                        <button type="submit" className="btn-primary">
+                            Login
+                        </button>
+                    </form>
+
+                    <p className="text-bottom">
+                        Don't have an account?{" "}
+                        <Link href="/" className="text-blue-500 hover:underline">
+                            Sign up page
+                        </Link>
+                    </p>
+
+                </div>
+            </div>
+
+        </div>
+    );
+}
