0723367253cd8fc718bd60f1ccfd38f50dac6561
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-04-23 01:32:40 +0800

    Initialise firebase admin - Modified frontend errors - Initialise npm install firebase-admin

--- trustpay/app/dashboard/user/page.tsx
+++ trustpay/app/dashboard/user/page.tsx
@@ -4,166 +4,228 @@ import { useState } from "react";
 import "./dashboard_user.css";
 
 type Transaction = {
-    id: number,
-    toName: string;
-    toPhone: string;
-    amount: number;
-    status: "NORMAL" | "SUSPICIOUS" | "FLAGGED";
+  id: number,
+  toName: string;
+  toPhone: string;
+  amount: number;
+  status: "NORMAL" | "SUSPICIOUS" | "FLAGGED";
 };
 
 export default function UserDashboard() {
 
-    const [showForm, setShowForm] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [loadingMessage, setLoadingMessage] = useState("");
 
-    const [toName, setToName] = useState("");
-    const [toPhone, setToPhone] = useState("");
-    const [amount, setAmount] = useState("");
+  const [showForm, setShowForm] = useState(false);
 
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [toName, setToName] = useState("");
+  const [toPhone, setToPhone] = useState("");
+  const [amount, setAmount] = useState("");
 
-    const handleSubmit = async () => {
-        const res = await fetch("/api/transfer", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                toName,
-                toPhone,
-                amount: Number(amount),
-            }),
-        });
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-        const result = await res.json();
+  const handleSubmit = async () => {
 
-        let status = result.status
+    console.log("Button 'transfer' clicked");
+    // validation for no input
+    if (!toName.trim() || !toPhone.trim() || !amount.trim()) {
+      alert("Please fill in all required fields.");
+      return;
+    }
 
-        if (status === "FLAGGED") {
-            alert("Transaction rejected: This transaction account was flagged and not allowed to transfer.");
-            setShowForm(false);
-            return;
-        }
+    // remove white spaces and check length
+    if (toPhone.replace(/\s/g, "").length < 9) {
+      alert("Phone number format wrong.")
+      return;
+    }
 
-        if (status === "SUSPICIOUS") {
-            alert("Transaction suspicious: Waiting admin to validate");
-        }
+    if (Number(amount) <= 0) {
+      alert("Transfer amount couldn't be zero.")
+      return;
+    }
 
-        if (status === "NORMAL") {
-            alert("Transaction Completed!");
-        }
+    try {
 
-        // Save transaction
-        setTransactions((prev) => [
-            {
-                id: Date.now(),
-                toName,
-                toPhone,
-                amount: Number(amount),
-                status,
-            },
-            ...prev,
-        ]);
+      setShowForm(false);
+      setLoading(true);
+      setLoadingMessage("Processing transaction...");
 
-        // Reset the form
-        setShowForm(false);
-        setToName("");
-        setToPhone("");
-        setAmount("");
-    };
+      await new Promise((r) => setTimeout(r, 400));
 
-    return (
-        <div className="dashboard-user">
+      setLoadingMessage("AI is analyzing transaction risk...");
+    
+      const res = await fetch("/api/transfer", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          toName,
+          toPhone,
+          amount: Number(amount),
+        }),
+      });
 
-            <h1 className="dashboard-title">
-                TrustPay Transaction View
-            </h1>
+      const result = await res.json();
 
-            <button
-                onClick={() => setShowForm(true)}
-                className="btn-primary"
-            >
-                + Add New Transaction
-            </button>
+      setLoading(false);
+      setLoadingMessage("");
 
-            {/* No transaction made before */}
-            {transactions.length === 0 ? (
-                <p className="text-gray-500">
-                    No transaction record found.
+      if (!res.ok) {
+        console.error("API ERROR:", result);
+        alert(result.error || "Server error occurred");
+        return;
+      }
+
+      console.log("RESULT:", result);
+
+      const status =
+        result.status === "approved"
+          ? "NORMAL"
+          : result.status === "pending"
+          ? "SUSPICIOUS"
+          : result.status === "rejected"
+          ? "FLAGGED"
+          : "SUSPICIOUS";
+
+      setTransactions((prev) => [
+        {
+          id: Date.now(),
+          toName,
+          toPhone,
+          amount: Number(amount),
+          status,
+        },
+        ...prev,
+      ]);
+
+      if (status === "FLAGGED") {
+        alert("Flagged account detected");
+      }
+
+      if (status === "SUSPICIOUS") {
+        alert("Suspicious transaction detected, this transaction has been sent to admin for validation, we'll notify you once the validation completed.");
+      }
+
+      if (status === "NORMAL") {
+        alert("Transaction Completed");
+      }
+
+      setToName("");
+      setToPhone("");
+      setAmount("");
+
+    } catch (error) {
+      console.error("ERROR:", error);
+      setLoading(false);
+      setLoadingMessage("");
+      alert("Error occurred");
+    }
+  };
+  
+  return (
+    <div className="dashboard-user">
+
+      <h1 className="dashboard-title">
+        TrustPay Transaction View
+      </h1>
+
+      <button
+        onClick={() => setShowForm(true)}
+        className="btn-primary"
+      >
+        + Add New Transaction
+      </button>
+
+      {/* No transaction made before */}
+      {transactions.length === 0 ? (
+        <p className="text-gray-500">
+          No transaction record found.
+        </p>
+
+        ) : (
+          <div className="transaction-list">
+
+            {transactions.map((tx) => (
+              <div key={tx.id} className="border p-4 rounded">
+                <p>To: {tx.toName}</p>
+                <p>Phone: {tx.toPhone}</p>
+                <p>Amount: RM {tx.amount}</p>
+
+                <p
+                  className={
+                    tx.status === "FLAGGED"
+                      ? "text-red-600"
+                      : tx.status === "SUSPICIOUS"
+                      ? "text-orange-500"
+                      : "text-green-600"
+                  }
+                >
+                  {tx.status}
                 </p>
+                
+              </div>
+            ))}
 
-            ) : (
-                <div className="transaction-list">
+          </div>
+        )}
 
-                    {transactions.map((tx) => (
-                        <div key={tx.id} className="border p-4 rounded">
-                            <p>To: {tx.toName}</p>
-                            <p>Phone: {tx.toPhone}</p>
-                            <p>Amount: RM {tx.amount}</p>
+        {/* form */}
+        {showForm && (
+          <div className="modal-bg">
+            <div className="modal-box">
 
-                            <p
-                                className={
-                                    tx.status === "FLAGGED"
-                                        ? "text-red-600"
-                                        : tx.status === "SUSPICIOUS"
-                                            ? "text-orange-500"
-                                            : "text-green-600"
-                                }
-                            >
-                                {tx.status}
-                            </p>
+              <h2 className="text-xl font-bold mb-4">
+                New Transaction
+              </h2>
 
-                        </div>
-                    ))}
+              <button
+                onClick={() => setShowForm(false)}
+                className="absolute top-3 right-3 text-black-600 hover:text-black text-xl"
+              >
+                x
+              </button>
 
-                </div>
-            )}
+              <input
+                className="input"
+                placeholder="Recipient Name"
+                value={toName}
+                onChange={(e) => setToName(e.target.value)}
+              />
 
-            {/* form */}
-            {showForm && (
-                <div className="modal-bg">
-                    <div className="modal-box">
+              <input
+                className="input"
+                placeholder="Phone Number"
+                value={toPhone}
+                onChange={(e) => setToPhone(e.target.value)}
+              />
 
-                        <h2 className="text-xl font-bold mb-4">
-                            New Transaction
-                        </h2>
+              <input
+                className="input"
+                placeholder="Amount($)"
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
 
-                        <button
-                            onClick={() => setShowForm(false)}
-                            className="absolute top-3 right-3 text-black-600 hover:text-black text-xl"
-                        >
-                            x
-                        </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    console.log("BUTTON PRESSED");
+                    handleSubmit();}}
+                  className="bg-blue-600 text-white px-3 py-1 rounded"
+                  
+                >
+                  Transfer
+                </button>
 
-                        <input
-                            className="input"
-                            placeholder="Recipient Name"
-                            value={toName}
-                            onChange={(e) => setToName(e.target.value)}
-                        />
+                {loading && (
+                  <div className="p-3 bg-blue-100 text-blue-700 rounded mb-4">
+                    {loadingMessage}
+                  </div>
+                )}
 
-                        <input
-                            className="input"
-                            placeholder="Phone Number"
-                            value={toPhone}
-                            onChange={(e) => setToPhone(e.target.value)}
-                        />
-
-                        <input
-                            className="input"
-                            placeholder="Amount($)"
-                            type="number"
-                            value={amount}
-                            onChange={(e) => setAmount(e.target.value)}
-                        />
-
-                        <button
-                            onClick={handleSubmit}
-                            className="bg-blue-600 text-white px-3 py-1 rounded"
-                        >
-                            Transfer
-                        </button>
-
-                    </div>
-                </div>
-            )}
-        </div>
+              </div>
+            </div>
+        )}
+      </div>
     );
-}
+  }
--- trustpay/app/signup/page.tsx
+++ trustpay/app/signup/page.tsx
@@ -7,88 +7,88 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/lib/firebase";
 
 export default function SignupPage() {
-    const [showPassword, setShowPassword] = useState(false);
-    const handleSignup = async (e: any) => {
-        e.preventDefault();
+  const [showPassword, setShowPassword] = useState(false);
+  const handleSignup = async (e: any) => {
+    e.preventDefault();
 
-        const form = e.currentTarget;
+    const form = e.currentTarget;
 
-        const email = form.email.value;
-        const password = form.password.value;
+    const email = form.email.value;
+    const password = form.password.value;
 
-        try {
-            await createUserWithEmailAndPassword(auth, email, password);
-            alert("Account created!");
-        } catch (error) {
-            alert("Fail to create account");
-        }
-    };
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("Account created!");
+    } catch (error) {
+      alert("Fail to create account");
+    }
+  };
 
-    return (
-        <div className="page-container">
+  return (
+    <div className="page-container">
 
-            <div className="image-section">
-                <img
-                    src="/title.png"
-                    alt="signup illustration"
-                    className="w-full h-full object-cover"
-                />
+      <div className="image-section">
+        <img
+          src="/title.png"
+          alt="signup illustration"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      
+      <div className="form-right">
+        <div className="form">
+          
+          <h1 className="title">SIGN UP PAGE</h1>
+          <p className="subtitle">Fill in registration details.</p>
+
+          <form className="flex flex-col gap-2" onSubmit={handleSignup}>
+            <label htmlFor="email" className="text-sm font-medium block">
+              Email
+            </label>
+            <input
+              name="email"
+              type="email"
+              id="email"
+              placeholder="Enter email address"
+              className="input-field"
+            />
+
+            {/* Password */}
+            <label htmlFor="password" className="text-sm font-medium block">
+              Password
+            </label>
+            <div className="relative">
+              <input
+                name="password"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="Enter password"
+                className="input-field"
+            />
+            <button
+                type="button"
+                className="absolute right-2 top-1/3 -translate-y-1/2 text-gray-500"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
+              </button>
             </div>
 
-            <div className="form-right">
-                <div className="form">
+            <button type="submit" className="btn-primary">
+              Sign up
+            </button>
+          </form>
 
-                    <h1 className="title">SIGN UP PAGE</h1>
-                    <p className="subtitle">Fill in registration details.</p>
-
-                    <form className="flex flex-col gap-2" onSubmit={handleSignup}>
-                        <label htmlFor="email" className="text-sm font-medium block">
-                            Email
-                        </label>
-                        <input
-                            name="email"
-                            type="email"
-                            id="email"
-                            placeholder="Enter email address"
-                            className="input-field"
-                        />
-
-                        {/* Password */}
-                        <label htmlFor="password" className="text-sm font-medium block">
-                            Password
-                        </label>
-                        <div className="relative">
-                            <input
-                                name="password"
-                                type={showPassword ? "text" : "password"}
-                                id="password"
-                                placeholder="Enter password"
-                                className="input-field"
-                            />
-                            <button
-                                type="button"
-                                className="absolute right-2 top-1/3 -translate-y-1/2 text-gray-500"
-                                onClick={() => setShowPassword(!showPassword)}
-                            >
-                                {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
-                            </button>
-                        </div>
-
-                        <button type="submit" className="btn-primary">
-                            Sign up
-                        </button>
-                    </form>
-
-                    <p className="text-bottom">
-                        Already have an account?{" "}
-                        <Link href="/login/user" className="text-blue-500 hover:underline">
-                            Login
-                        </Link>
-                    </p>
-
-                </div>
-            </div>
+          <p className="text-bottom">
+            Already have an account?{" "}
+            <Link href="/login/user" className="text-blue-500 hover:underline">
+              Login
+            </Link>
+          </p>
 
         </div>
-    );
+     </div>
+
+    </div>
+  );
 }
