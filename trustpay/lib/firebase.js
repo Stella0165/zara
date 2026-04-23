96f6244928ce8a786b4848f2a3887075ab5d2a96
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-04-23 22:39:34 +0800

    Remove unnecessary file, modified firebase.js for gemini returning style

--- trustpay/lib/firebase.js
+++ trustpay/lib/firebase.js
@@ -3,20 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBe3fwd02ym5tZSnNr2KRS-8M858jDbelM",
-    authDomain: "trustpay-d614d.firebaseapp.com",
-    projectId: "trustpay-d614d",
-    storageBucket: "trustpay-d614d.firebasestorage.app",
-    messagingSenderId: "629239117053",
-    appId: "1:629239117053:web:cee8a4b3dd640a7d603a94"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
